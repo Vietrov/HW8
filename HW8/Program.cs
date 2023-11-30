e5221b86b6de6f505a620a53fe85cd35f927766e
PC <PC@DESKTOP-R4P1PJC>
2023-11-30 22:59:16 +0100

    Add project files.

---
+++ HW8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Lesson
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+             List<string> tasks = new List<string>();
+
+            tasks.Add("Встать в 8 утра");
+            tasks.Add("позавтракать");
+            tasks.Add("пойти на работу");
+
+            tasks.Insert(1,"почистить зубы");
+
+            tasks.RemoveAt(3);
+            foreach (string task in tasks)
+            {
+                Console.WriteLine(task);
+            }
+
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
